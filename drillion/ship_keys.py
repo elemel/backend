96a08a9c7b11ce96de1c2034efcc19622c4eb419
Mikael Lind <elemel@elemel.se>
2013-10-27 23:09:05 +0100

    Change second ship controls to IJKL

--- drillion/ship_keys.py
+++ drillion/ship_keys.py
@@ -1,8 +1,8 @@
 from pyglet.window import key
 
-PLAYER_SHIP_KEYS = dict(left=[key.A, key.LEFT], right=[key.D, key.RIGHT],
-                        thrust=[key.W, key.UP], fire=[key.S, key.DOWN])
+PLAYER_SHIP_KEYS = dict(left=[key.A, key.J], right=[key.D, key.L],
+                        thrust=[key.W, key.I], fire=[key.S, key.K])
 PLAYER_1_SHIP_KEYS = dict(left=[key.A], right=[key.D], thrust=[key.W],
                           fire=[key.S])
-PLAYER_2_SHIP_KEYS = dict(left=[key.LEFT], right=[key.RIGHT],
-                          thrust=[key.UP], fire=[key.DOWN])
+PLAYER_2_SHIP_KEYS = dict(left=[key.J], right=[key.L], thrust=[key.I],
+                          fire=[key.K])
