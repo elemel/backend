25a015b261b2acb1e891f5cfb649d2c32dac40a6
Mikael Lind <elemel@elemel.se>
2013-10-31 18:00:35 +0100

    Optimize physics component

--- drillion/physics_component.py
+++ drillion/physics_component.py
@@ -7,9 +7,9 @@ class PhysicsComponent(Component):
                  angular_velocity=0.0, angular_acceleration=0.0):
         super(PhysicsComponent, self).__init__()
 
-        self.position = Vector2(*position)
-        self.velocity = Vector2(*velocity)
-        self.acceleration = Vector2(*acceleration)
+        self.position = tuple(position)
+        self.velocity = tuple(velocity)
+        self.acceleration = tuple(acceleration)
 
         self.angle = angle
         self.angular_velocity = angular_velocity
@@ -18,6 +18,8 @@ class PhysicsComponent(Component):
         self.transform_component = transform_component
         self.update_phase = update_phase
 
+        self._transform = Transform2()
+
     def create(self):
         self.update_phase.add_handler(self)
 
@@ -25,14 +27,24 @@ class PhysicsComponent(Component):
         self.update_phase.add_handler(self)
 
     def update(self, dt):
-        self.position += dt * self.velocity
-        self.velocity += dt * self.acceleration
+        x, y = self.position
+        dx, dy = self.velocity
+        ddx, ddy = self.acceleration
+
+        x += dt * dx
+        y += dt * dy
+        dx += dt * ddx
+        dy += dt * ddy
 
         self.angle += dt * self.angular_velocity
         self.angular_velocity += dt * self.angular_acceleration
 
-        transform = Transform2(*self.transform_component.transform)
-        transform.reset()
-        transform.rotate(self.angle)
-        transform.translate(*self.position)
-        self.transform_component.transform = transform
+        self.position = x, y
+        self.velocity = dx, dy
+
+        self._transform.reset()
+        if self.angle != 0.0:
+            self._transform.rotate(self.angle)
+        if x != 0.0 or y != 0.0:
+            self._transform.translate(x, y)
+        self.transform_component.transform = self._transform
