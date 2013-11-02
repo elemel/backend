756f16e2cfc5c69821c3261028544011f5190f3b
Mikael Lind <elemel@elemel.se>
2013-11-02 09:43:19 +0100

    Change ship control keys

--- drillion/ship_keys.py
+++ drillion/ship_keys.py
@@ -1,6 +1,6 @@
 from pyglet.window import key
 
-PLAYER_1_SHIP_KEYS = dict(left=[key.A], right=[key.D], thrust=[key.S],
-                          fire=[key.W, key.LSHIFT])
+PLAYER_1_SHIP_KEYS = dict(left=[key.A], right=[key.D],
+                          thrust=[key.S, key.LSHIFT], fire=[key.W, key.SPACE])
 PLAYER_2_SHIP_KEYS = dict(left=[key.J], right=[key.L], thrust=[key.K],
                           fire=[key.I])
